7c9edfe2568ecd7bd4b4a32641b18b38ab34ae36
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-03 17:53:33 -0600

    removed number of sequences method as it is no longer needed

--- ruby/fastq_reader/lib/fastq_reader.rb
+++ ruby/fastq_reader/lib/fastq_reader.rb
@@ -10,16 +10,8 @@ class FastqReader
 
   def select_directory_files
     files = Dir.glob("#{@directory}/*.fastq")
-    binding.pry
   end
 
-  def number_of_sequences
-    num_seq_list = select_directory_files.map do |file|
-      file_data = File.readlines(file)
-      num_seq = file_data.each_slice(4).to_a.count
-    end
-    directory_data = select_directory_files.zip(num_seq_list)
-  end
 
   def percent_seq_over_30
     percents_list = select_directory_files.map do |file|
--- ruby/fastq_reader/test/fastq_reader_test.rb
+++ ruby/fastq_reader/test/fastq_reader_test.rb
@@ -1,4 +1,4 @@
-require "./lib/fast_reader"
+require "./lib/fastq_reader"
 require "minitest/autorun"
 require "minitest/pride"
 
@@ -23,22 +23,15 @@ class FastqReaderTest < Minitest::Test
     assert_equal ["read2/Sample_R2.fastq"], fastq.select_directory_files
   end
 
-  def test_it_can_return_numer_of_sequences_in_partucular_file
-    dir_name = ("read3")
-    fastq = FastqReader.new(dir_name)
-    assert_equal [["read3/sample_2.fastq", 2], ["read3/sample.fastq", 2]], fastq.number_of_sequences
-  end
-
   def test_it_can_return_percentage_of_sequences_over_30
     dir_name = ("read3")
     fastq = FastqReader.new(dir_name)
-    assert_equal [["read3/sample_2.fastq", 50.0], ["read3/sample.fastq",50.0]], fastq.percent_seq_over_30
+    assert_equal [["read3/sample_2.fastq", 25.0], ["read3/sample.fastq",50.0]], fastq.percent_seq_over_30
   end
 
   def test_it_can_return_clean_data
     dir_name = ("read3")
     fastq = FastqReader.new(dir_name)
-    binding.pry
-    assert_equal "something", fastq.data_display
+    assert_equal [["read3/sample_2.fastq", 25.0], ["read3/sample.fastq", 50.0]], fastq.data_display
   end
 end
