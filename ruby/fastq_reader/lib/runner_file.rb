a1129ed20303bfa8c7ac350059c36e6f4c0698f4
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-03 17:42:12 -0600

    updated runner file

--- ruby/fastq_reader/lib/runner_file.rb
+++ ruby/fastq_reader/lib/runner_file.rb
@@ -1,6 +1,7 @@
 require "./lib/fast_reader"
 
-puts "Welcome to the FastQ file reader"
+puts "Welcome to the FastQ file reader!"
+puts "please place directories needed for analysis in the fastq_reader directory."
 puts "please type in the name of the Directory you wish to analyze"
 directory = gets.chomp
 data = FastqReader.new(directory)
