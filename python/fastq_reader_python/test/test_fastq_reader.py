5ce2356049405b1beee21906f390f05eac86619a
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-08 16:08:38 -0600

    having world war three with xcode not supporting the most recent python verison

--- python/fastq_reader_python/test/test_fastq_reader.py
+++ python/fastq_reader_python/test/test_fastq_reader.py
@@ -5,8 +5,8 @@ import fastq_reader
 class TestFastqReader(unittest.TestCase):
 
     def test_it_exists(self):
-        fastq_reader = FastqReader.new
-        self.assertIsInstance(fastq_reader, FastqReader)
+        fastq_reader = FastqReader()
+        self.assertIsInstance(fastq_reader, FastqReader())
 
 
 if __name__ == '__main__':
